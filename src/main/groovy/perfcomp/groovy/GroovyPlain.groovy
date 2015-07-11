581470d0ebe6fe0ee744fb110d552320076c8068
Keegan Witt <keeganwitt@gmail.com>
2015-07-10 20:51:11 -0400

    performance improvements & switched from Gradle to Maven

--- src/main/groovy/perfcomp/groovy/GroovyPlain.groovy
+++ src/main/groovy/perfcomp/groovy/GroovyPlain.groovy
@@ -1,27 +1,37 @@
 package perfcomp.groovy
 
-import groovy.transform.Immutable
+import groovy.transform.CompileStatic
 import groovy.transform.TupleConstructor
 
 /**
  * Created by rahul on 7/10/15.
  */
 @TupleConstructor
+@CompileStatic
 class Person {
     String name
 }
+@CompileStatic
 class Parent extends Person {
     Parent(String name) { super(name) }
 }
+@CompileStatic
 class Child extends Person {
     Child(String name) {super(name)}
 }
 @TupleConstructor
+@CompileStatic
 class Dog {
     String name
+
+    Dog(String name) {
+        this.name = name
+    }
 }
 class Util {
+    @CompileStatic
     static String printName(Person person) { "printName(Person): $person.name" }
+    @CompileStatic
     static String printName(Child child) { "printName(Child): $child.name" }
     static String printName(p) { "printName(p): $p.name" }
 }
